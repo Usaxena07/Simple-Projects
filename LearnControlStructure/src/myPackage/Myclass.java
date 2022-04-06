156e9df8fb7e6b6581087da41eceb8e8d13bfc41
Uday Saxena <102949443+Usaxena07@users.noreply.github.com>
2022-04-06 16:02:20 +0530

    Loop

---
+++ LearnControlStructure/src/myPackage/Myclass.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class Myclass 
+{
+
+	public static void main(String[] args) 
+	{
+		int i=1;
+		
+		for (i=1;i<=5;i++)
+			System.out.println("Hello");
+			
+
+	}
+
+}
